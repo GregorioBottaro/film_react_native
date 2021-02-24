a87e2611f2d4d341082ae0905780597134905198
amandine <amandine.donat.filliod@gmail.com>
2021-02-24 13:33:32 +0100

    24.02 version

--- src/components/ListResults.js
+++ src/components/ListResults.js
@@ -1,35 +1,38 @@
-import React from 'react'
-import {FlatList, StyleSheet, Text, View} from 'react-native'
-import { Card } from './Card'
-import data from '../helpers/filmDatas'
-import { EmptyResults } from './EmptyResults'
+import React from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { Card } from "./Card";
+import { data } from "../helpers/filmDatas";
+import { EmptyResults } from "./EmptyResults";
 
 export const ListResults = (props) => {
   return (
     <View style={styles.container}>
-    {
-      data.length > 0 ? (
+      {data.length > 0 ? (
         <View style={styles.listContainer}>
-          <FlatList 
+          <FlatList
             data={data}
-            keyExtractor={key => String(key.id)}
-            renderItem={({ item }) => <Card key={item.id} title={item.title} release_date={item.release_date} />}
-            />
+            keyExtractor={(key) => String(key.id)}
+            renderItem={({ item }) => (
+              <Card
+                key={item.id}
+                title={item.title}
+                release_date={item.release_date}
+              />
+            )}
+          />
         </View>
-        ) : (
-          <EmptyResults />
-        )
-      }
-  </View>
-  )
-}
-
+      ) : (
+        <EmptyResults />
+      )}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
-      flex: 5
+    flex: 5,
   },
   listContainer: {
     margin: 10,
-  }
-})
+  },
+});
--- src/helpers/filmDatas.js
+++ src/helpers/filmDatas.js
@@ -1,4 +1,4 @@
-export default data = [
+export const data = [
     {
         id:181808,
         vote_average:7.2,
