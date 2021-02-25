c5e0b6e37439f2b0e63eea910180ce0a94619e61
amandine <amandine.donat.filliod@gmail.com>
2021-02-25 09:58:39 +0100

    app.js modif

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { SafeAreaView, StatusBar } from 'react-native';
-import { ListResults } from './src/components/ListResults';
-import {Search} from "./src/components/Search";
+import React from "react";
+import { SafeAreaView, StatusBar } from "react-native";
+import { ListResults } from "./src/components/ListResults";
+import { Search } from "./src/components/Search";
 
 export default function App() {
   return (
-      <>
-        <StatusBar backgroundColor="green" barStyle={"light-content"} />
-        <Search />
-        <ListResults />
-      </>
+    <>
+      <StatusBar backgroundColor="green" barStyle={"light-content"} />
+      <Search />
+      <ListResults />
+    </>
   );
 }
